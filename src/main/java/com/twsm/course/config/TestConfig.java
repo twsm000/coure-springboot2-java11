9363b6586779c32e539653a5bbc30d47034fd98e
Thomas M <twsm000@gmail.com>
2020-12-03 21:14:14 -0300

    Add Category

--- src/main/java/com/twsm/course/config/TestConfig.java
+++ src/main/java/com/twsm/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.twsm.course.entities.Category;
 import com.twsm.course.entities.Order;
 import com.twsm.course.entities.User;
 import com.twsm.course.entities.enums.OrderStatus;
+import com.twsm.course.repositories.CategoryRepository;
 import com.twsm.course.repositories.OrderRepository;
 import com.twsm.course.repositories.UserRepository;
 
@@ -22,9 +24,19 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private OrderRepository orderRepository;
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
+        
+        Category c1 = new Category(null, "Eletronics");
+        Category c2 = new Category(null, "Books");
+        Category c3 = new Category(null, "Computers");
+        
+        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+        
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "12345678", "4328765");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "43218765", "'1234567");
 

b2ae866a64434a20ae5760cbc760df419dd5a14e
Thomas M <twsm000@gmail.com>
2020-12-04 22:02:26 -0300

    Exception handling - delete

--- src/main/java/com/twsm/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/twsm/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.twsm.course.services.exceptions.DatabaseException;
 import com.twsm.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -17,10 +18,19 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(
             ResourceNotFoundException e, HttpServletRequest request) {
-        String error = "Resource not found";
+        String message = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError exception = new StandardError(Instant.now(),
-                status.value(), error, e.getMessage(), request.getRequestURI());
-        return new ResponseEntity<StandardError>(exception, status);
+        StandardError error = new StandardError(Instant.now(),
+                status.value(), message, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+    
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String message = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = new StandardError(Instant.now(),
+                status.value(), message, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
     }
 }
--- src/main/java/com/twsm/course/services/UserService.java
+++ src/main/java/com/twsm/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.twsm.course.entities.User;
 import com.twsm.course.repositories.UserRepository;
+import com.twsm.course.services.exceptions.DatabaseException;
 import com.twsm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,13 @@ public class UserService {
     }
     
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage()); 
+        }
     }
     
     public User update(Long id, User obj) {
